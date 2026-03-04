8363e39bb95713901e1d0cbc29114a6544a46eaa
Alex Stroie <alexander872009@gmail.com>
2026-03-04 13:17:21 +0200

    Frontend

--- CP_PRT/src/app/pages/home/home.component.ts
+++ CP_PRT/src/app/pages/home/home.component.ts
@@ -1,18 +1,30 @@
 import {Component} from '@angular/core';
-import {Router} from '@angular/router';
+import {Router, RouterLink} from '@angular/router';
 import {CommonModule} from '@angular/common';
 import {TranslatePipe} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, TranslatePipe],
+  imports: [CommonModule, TranslatePipe, RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
 export class HomeComponent {
 
+  openFaq: number | null = null;
+
+  faq = [
+    { q: 'HOME.FAQ.Q1', a: 'HOME.FAQ.A1' },
+    { q: 'HOME.FAQ.Q2', a: 'HOME.FAQ.A2' },
+    { q: 'HOME.FAQ.Q3', a: 'HOME.FAQ.A3' }
+  ];
+
   constructor(private router: Router) {}
+
+  toggleFaq(index: number) {
+    this.openFaq = this.openFaq === index ? null : index;
+  }
 
   navigateToURL(url: string): void {
     this.router.navigate([url])
